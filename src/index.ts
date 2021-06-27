254b9eab32daf76d036c64e11d0844e532adcf24
siwan <pswan3168@gmail.com>
2021-06-27 15:27:27 +0900

    tsc-watch로 실시간 컴파일 햅괴

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
 }
 
-console.log(sayHi('siwan', 27, 'male'))
+console.log(sayHi('siwan', 27, 'male'));
+
+export {};
