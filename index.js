a1b1f00fe4d138a700e05517942b5de726e223c6
siwan <pswan3168@gmail.com>
2021-06-27 15:02:39 +0900

    TypeScript로 명시적인 데이터타입 선언

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-alert("hello");
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+console.log(sayHi('siwan', 27, 'male'));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-alert("hello")
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+}
+
+console.log(sayHi('siwan', 27, 'male'))
