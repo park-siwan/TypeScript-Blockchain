9f75a89497c5b3e7cfdc3900eb1967b7d212e7db
park-siwan <pswan3168@gmail.com>
2021-06-28 16:07:14 +0900

    블록체인 구조검증, 해쉬검증 하기

--- src/blockChain.ts
+++ src/blockChain.ts
@@ -1,13 +1,21 @@
 import * as CryptoJS from "crypto-js";
 
 class Block {
-  public index:number;
-  public hash: string;
-  public previousHash: string;
-  public data: string;
-  public timestamp: number;
 
   static calculateBlockHash = (index:number, previousHash:string, timestamp:number, data:string) :string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  static validateStructure = (aBlock: Block) : boolean => 
+    typeof aBlock.index === "number" && 
+    typeof aBlock.hash === "string" && 
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" && 
+    typeof aBlock.data === "string";
+
+    public index:number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
 
   constructor(
     index:number,
@@ -56,6 +64,29 @@ const createNewBlock = (data:string) : Block => {
     return newBlock;
 };
 
-console.log(createNewBlock("hello"), createNewBlock("bye bye"))
+const getHashforBlock = (aBlock: Block) :string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
+const isBlockValid = (candidateBlock : Block, previousBlock: Block) : boolean => {
+  if(Block.validateStructure(candidateBlock)){
+    return false;
+  } else if(previousBlock.index + 1 !== candidateBlock.index){
+    return false;
+  } else if(previousBlock.hash !== candidateBlock.previousHash){
+    return false;
+  } else if(getHashforBlock(candidateBlock) !== candidateBlock.hash){
+    return false;
+  } else {
+    return true;
+  }
+};
+
+
+// console.log(createNewBlock("hello"), createNewBlock("bye bye"))
+
+const addBlock = (candidateBlock: Block) : void => {
+  if(isBlockValid(candidateBlock, getLatestBlock())){
+    blockchain.push(candidateBlock)
+  }
+}
 
 export {};
