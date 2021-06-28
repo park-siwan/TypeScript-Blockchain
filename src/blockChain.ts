7cbca73a78468826b135786a47fa055864d8a110
park-siwan <pswan3168@gmail.com>
2021-06-28 15:08:19 +0900

    블록체인 만들기 2

--- src/blockChain.ts
+++ src/blockChain.ts
@@ -1,9 +1,14 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index:number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (index:number, previousHash:string, timestamp:number, data:string) :string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
     index:number,
     hash: string,
@@ -19,9 +24,17 @@ class Block {
     }
 }
 
+
+
 const genesisBlock:Block = new Block(0, "2423432423", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 console.log(blockchain)
 
